4714d8aaee616c62afe1a641aebe254c1f0d3015
Zia Ur Rehman <ziaishere@gmail.com>
2016-05-29 00:17:42 +0530

    initial commit

---
+++ src/main/java/com/ziahub/javafx/App.java
@@ -0,0 +1,28 @@
+package com.ziahub.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("First JavaFX App");
+		
+		Label label = new Label("Hello World, JavaFX !");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+        
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
